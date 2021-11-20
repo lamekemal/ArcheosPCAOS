c42f7bc5f0f1f5ceac0b6ea6563aba2645ce18f0
Lamekemal <handill@live.fr>
2021-11-21 00:59:42 +0100

    pietry

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import OpenPrivacy from "./privacy-open";
+import Home from "./Home";
+import Bumper from "./Bumper";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/openapps-privacy" element={<OpenPrivacy />} />
+        <Route exact path="/merged-apps" element={<Bumper />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
